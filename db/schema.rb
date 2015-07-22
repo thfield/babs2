6a81f856eba16550c3eb246b9ed30b6247dc64b2
thfield <thfield@gmail.com>
2015-07-22 14:49:21 -0700

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722061232) do
+ActiveRecord::Schema.define(version: 20150722204549) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "stations", force: :cascade do |t|
     t.text     "data_dump"
